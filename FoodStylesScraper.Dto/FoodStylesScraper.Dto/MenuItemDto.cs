7509e78aa3fd3f999b7f45f930c89a5a5a0a9552
Ferenc Vigh <ferenc.vigh@principal33.com>
2020-09-27 13:40:50 +0300

    created solution

---
+++ FoodStylesScraper.Dto/FoodStylesScraper.Dto/MenuItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodStylesScraper.Dto
+{
+    public class MenuItemDto
+    {
+        public string MenuTitle { get; set; }
+        public string MenuDescription { get; set; }
+        public string MenuSectionTitle { get; set; }
+        public string DishName { get; set; }
+        public string DishDescription { get; set; }
+    }
+}
---
+++ FoodStylesScraper/Controllers/ScrapeController.cs
@@ -0,0 +1,51 @@
+﻿using FoodStylesScraper.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace FoodStylesScraper.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ScrapeController : ControllerBase
+    {
+        private readonly ILogger<ScrapeController> logger;
+
+        public ScrapeController(ILogger<ScrapeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ScrapeParameters parameters)
+        {
+            try
+            {
+                var result = new List<MenuItemDto>
+                {
+                    new MenuItemDto
+                    {
+                        MenuTitle = "Breakfast",
+                        MenuDescription = "Our nutritious breakfasts are served in seconds and last until lunch...",
+                        MenuSectionTitle = "Super Eggs",
+                        DishName = "Super Eggs",
+                        DishDescription = "Free-range egg omelette, rolled and filled with avocado, roquito peppers, edamame, spinach and free-range egg mayonnaise."
+                    }
+                };
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error scraping menu at {parameters.MenuUrl}", ex);
+                return BadRequest();
+            }
+        }
+    }
+
+    public class ScrapeParameters
+    {
+        public string MenuUrl { get; set; }
+    }
+}
